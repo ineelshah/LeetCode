8e73c6507eed6e9cc645131d21fbcd2e6dd31ddc
Neel Shah <neelshah.2798@gmail.com>
2020-08-06 12:44:26 +0530

    Day 6

---
+++ August/Day_6_Find_All_Duplicates_In_An_Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer> ans = new ArrayList<>();
+        for(int i = 0; i < nums.length - 1; i++)
+        {
+            
+            if(nums[i] == nums[i + 1])
+                ans.add(nums[i]);
+            
+        }
+        return ans;
+    }
+}
