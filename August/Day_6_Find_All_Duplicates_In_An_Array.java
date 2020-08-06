58da4fc32bf649a2f24da332c6b3f43eda104d45
Neel Shah <neelshah.2798@gmail.com>
2020-08-06 13:33:24 +0530

    Day 6 Optimal

--- August/Day_6_Find_All_Duplicates_In_An_Array.java
+++ August/Day_6_Find_All_Duplicates_In_An_Array.java
@@ -1,3 +1,27 @@
+// The trick is to negate the value in the array, which is at the index {curr_number - 1}, since all elements in  the array are from 1 to the // size of the array.
+// Refer: NICK WHITE's YouTube video for more details.
+
+
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        
+        List<Integer> ans = new ArrayList<>();
+        
+        for(int i = 0; i < nums.length ; i++)
+        {
+            int index = Math.abs(nums[i]) - 1;
+            if(nums[index] < 0)
+                ans.add(Math.abs(nums[i]));
+            
+            nums[index] *= -1;
+            
+        }
+        return ans;
+    }
+}
+
+
+/*
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         Arrays.sort(nums);
@@ -12,3 +36,4 @@ class Solution {
         return ans;
     }
 }
+*/
