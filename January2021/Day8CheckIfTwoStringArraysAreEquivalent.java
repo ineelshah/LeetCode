48b9c7e94bd48f6987c119d1c397f8ac51a1470f
Neel Shah <neelshah.2798@gmail.com>
2021-01-11 20:31:10 +0530

    Day 8

---
+++ January2021/Day8CheckIfTwoStringArraysAreEquivalent.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        
+        for(String subStr: word1) {
+            sb1.append(subStr);
+        }
+        
+        for(String subStr: word2) {
+            sb2.append(subStr);
+        }
+        
+        return sb1.toString().equals(sb2.toString());
+        
+    }
+}
